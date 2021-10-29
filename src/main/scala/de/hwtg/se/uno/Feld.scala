e01cbababcb393d21d3b2b30acb3375c2452e1c7
haasentimo <haasentimo@gmail.com>
2021-10-29 12:11:39 +0200

    Table

--- src/main/scala/de/hwtg/se/uno/Feld.scala
+++ src/main/scala/de/hwtg/se/uno/Feld.scala
@@ -19,6 +19,8 @@
   println
   println("xxxxxxxxxxxxxxxxxxxxxx")
   print(finalCard(7))
+  println("xxxxxxxxxxxxxxxxxxxxxx")
+  println(Table(3, 7))
 
 def row(): String = //tested
   "+" + "-" * 2
@@ -51,3 +53,9 @@ def midRow(cCount: Int): String = //tested
 //creates the card row
 def finalCard(cCount: Int): String = //tested
   udRow(cCount) + midRow(cCount) + udRow(cCount)
+
+def stack(): String =
+  finalCard(1) + "\n"
+
+def Table(cP1: Int, cP2: Int): String =
+  finalCard(cP1) + "\n" + stack() + finalCard(cP2) + "\n"
