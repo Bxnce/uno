f3ce42cd210934d76e94909299ccae29fa5c0622
haasentimo <haasentimo@gmail.com>
2021-11-05 10:46:46 +0100

    Tui

--- src/main/scala/de/htwg/se/uno/Main.scala
+++ src/main/scala/de/htwg/se/uno/Main.scala
@@ -4,12 +4,17 @@ import Card._
 import CardStack._
 import scala.collection.immutable.HashMap
 import Field._
+import de.htwg.se.uno._
 
 @main def Main: Unit =
   var stack = fill()
 
+  /*
   val p1 = readLine("Player1: ")
   val p2 = readLine("Player2: ")
   val cards = readLine("Kartenanzahl: ").toInt
 
   print(table(p1, p2, cards, cards))
+   */
+  val game1 = new TUI()
+  game1.TUI()
