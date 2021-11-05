2dc1a5ac2779680004c56a1be8dafdcfaad39ea8
haasentimo <haasentimo@gmail.com>
2021-11-05 14:08:31 +0100

    main

--- src/main/scala/de/htwg/se/uno/Main.scala
+++ src/main/scala/de/htwg/se/uno/Main.scala
@@ -5,9 +5,6 @@ import aview._
 //import controller._
 import model._
 
-import scala.collection.immutable.HashMap
-import de.htwg.se.uno._
-
 @main def Main: Unit =
   println("Willkommen zu Uno!\n")
   val game1 = TUI()
