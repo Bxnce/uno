927860f00ca9e4b2fd64eb1958b87564412273b6
haasentimo <haasentimo@gmail.com>
2021-11-10 17:19:10 +0100

    PlayerSpec + Comments in Player

--- src/main/scala/de/htwg/se/uno/model/Player.scala
+++ src/main/scala/de/htwg/se/uno/model/Player.scala
@@ -6,9 +6,9 @@ import CardLayout._
 case class Player(n: String) {
   val name = n
   var karten = Vector[Card]()
-  override def toString: String = name
+  override def toString: String = name //wofür brauchen wir die ToString ?
 
-  def print(): String =
+  def print(): String = //printet bei einen leeren Vektor trotzdem noch eine Karte aber ohne Wert
     if (karten.size == 0) {
       return udRow(1) + "|  |" + eol + udRow(1)
     } else {
@@ -20,6 +20,7 @@ case class Player(n: String) {
   def getName(): String =
     return name
 
+  //Was passiert wenn man eine falsche Karte einfügen will
   def add(karte: Card) =
     karten = karten :+ karte
 }
--- src/test/scala/de/htwg/se/uno/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/uno/PlayerSpec.scala
@@ -4,12 +4,58 @@ package model
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers._
 
+import Card._
+
 class PlayerSpec extends AnyWordSpec {
   "Player" should {
     "have name in form of a String" in {
       val p1 = Player("Spieler1")
       p1.name shouldBe ("Spieler1")
     }
-    "have a Vector with the Players Cards" in {}
+    "have a Vector with the Players Cards" in {
+      val p1 = Player("Spieler1")
+      p1.karten.size shouldBe (0)
+      p1.add(R0)
+      p1.karten.size shouldBe (1)
+      p1.karten(0) shouldBe (R0)
+    }
+    "override toString and print the name of the Player" in {
+      val p1 = Player("Spieler1")
+      p1.toString shouldBe ("Spieler1")
+    }
+    "have a method print() that prints out the Players Cards" in {
+      val p1 = Player("Spieler1")
+      p1.print() shouldBe (
+        "+--+\n" +
+          "|  |\n" +
+          "+--+\n"
+      )
+      p1.add(R0)
+      p1.print() shouldBe (
+        "+--+\n" +
+          "|R0|\n" +
+          "+--+\n"
+      )
+      p1.add(Y9)
+      p1.print() shouldBe (
+        "+--+--+\n" +
+          "|R0|Y9|\n" +
+          "+--+--+\n"
+      )
+    }
+    "have a method getName() that returns the name of the Player" in {
+      val p1 = Player("Spieler1")
+      p1.getName() shouldBe ("Spieler1")
+    }
+    "have a method add(Card) that adds a card to the Vector karten" in {
+      val p1 = Player("Spieler1")
+      p1.karten.size shouldBe (0)
+      p1.add(R0)
+      p1.karten.size shouldBe (1)
+      p1.karten(0) shouldBe (R0)
+      p1.add(Y9)
+      p1.karten.size shouldBe (2)
+      p1.karten(1) shouldBe (Y9)
+    }
   }
 }
