f5ef60721241b090710206ffd7646984e0453b1d
haasentimo <haasentimo@gmail.com>
2021-11-11 17:37:38 +0100

    test

--- src/test/scala/de/htwg/se/uno/model/GameSpec.scala
+++ src/test/scala/de/htwg/se/uno/model/GameSpec.scala
@@ -25,6 +25,104 @@ class GameSpec extends AnyWordSpec {
       game.p2.karten(0) shouldBe (G7)
       //die ganzen Abfragen prüfen
       game.add("p2", "XX") shouldEqual (-1)
+      game.add("p1", "R1")
+      game.add("p1", "R1")
+      game.add("p1", "R1") shouldEqual (-2)
+      game.add("p3", "G8") shouldEqual (-3)
+      game.cardStack.cards(R2) shouldBe (2)
+      game.add("p1", "R2")
+      game.cardStack.cards(R2) shouldBe (1)
+    }
+    "have a method add(String, Card) that adds a card to a players hand" in {
+      val game = Game("player1", "player2", 0)
+      game.p1.karten.size shouldBe (0)
+      game.p2.karten.size shouldBe (0)
+      game.midCard.karten.size shouldBe (1)
+
+      game.add("p1", XX)
+      game.p1.karten.size shouldBe (1)
+
+      game.add("p2", R0) shouldEqual (0)
+      game.p2.karten.size shouldBe (1)
+
+      game.add("midstack", R0)
+      game.midCard.karten.size shouldBe (2) //es liegen dann halt immer mehrere Karten auf dem Stapel
+
+      game.add("p3", "G8") shouldEqual (-3)
+    }
+    "have a method take(String) that adds a random card to a players hand" in {
+      val game = Game("player1", "player2", 0)
+      game.p1.karten.size shouldBe (0)
+      game.p2.karten.size shouldBe (0)
+
+      game.take("p1")
+      game.p1.karten.size shouldBe (1)
+
+      game.take("p2")
+      game.p2.karten.size shouldBe (1)
+    }
+    "have a method take() that adds a random card to the player whos turn it is" in {
+      val game = Game("player1", "player2", 0)
+      game.p1.karten.size shouldBe (0)
+      game.p2.karten.size shouldBe (0)
+      game.playerDiff shouldBe (0)
+
+      game.take()
+      game.p1.karten.size shouldBe (1)
+
+      game.next()
+      game.playerDiff shouldBe (1)
+      game.take()
+      game.p2.karten.size shouldBe (1)
+    }
+    //place funktioniert nach next nicht mehr richtig muss noch getestet werden
+    "have a method next() that increments the PlayerDiff variable" in {
+      val game = Game("player1", "player2", 0)
+      game.playerDiff shouldBe (0)
+      game.next()
+      game.playerDiff shouldBe (1)
+      game.next()
+      game.playerDiff shouldBe (2)
+    }
+    "override the method toString() and return a String in Form of" in {
+      val game = Game("player1", "player2", 0)
+      game.midCard.karten = game.midCard.karten.updated(0, R0)
+      game.toString() shouldBe (
+        "player1" + eol +
+          "+--+" + eol +
+          "|  |" + eol +
+          "+--+" + eol +
+          eol +
+          "+--+" + eol +
+          "|R0|" + eol +
+          "+--+" + eol +
+          eol +
+          "+--+" + eol +
+          "|  |" + eol +
+          "+--+" + eol +
+          "player2" + eol
+      )
+
+      val game1 = Game("player1", "player2", 1)
+      game1.midCard.karten = game.midCard.karten.updated(0, R0)
+      game1.p1
+      //game1.p1.karten = game.p1.karten.updated(0, B0)
+      //game1.p2.karten = game.p2.karten.updated(0, G0)
+      game1.toString() shouldBe (
+        "player1" + eol +
+          "+--+" + eol +
+          "|B0|" + eol +
+          "+--+" + eol +
+          eol +
+          "+--+" + eol +
+          "|R0|" + eol +
+          "+--+" + eol +
+          eol +
+          "+--+" + eol +
+          "|G0|" + eol +
+          "+--+" + eol +
+          "player2" + eol
+      )
     }
   }
 }
--- src/test/scala/de/htwg/se/uno/model/test.worksheet.sc
+++ src/test/scala/de/htwg/se/uno/model/test.worksheet.sc
@@ -12,3 +12,40 @@ p1.karten.size
 p1.karten(0)
 p1.print()
 p1.add(R1)
+
+import de.htwg.se.uno.model.Game
+import de.htwg.se.uno.model.Player._
+import de.htwg.se.uno.model.toCard._
+import de.htwg.se.uno.model.CardLayout._
+import de.htwg.se.uno.model.Card._
+
+val game = Game("player1", "player2", 0)
+game.p1.karten.size
+game.p2.karten.size
+game.midCard.karten.size
+game.midCard.karten(0)
+game.midCard.karten = game.midCard.karten.updated(0, R0)
+game.midCard.karten.toString()
+
+game.add("p1", XX)
+game.p1.karten.size
+game.p1.karten(0)
+
+game.add("p2", R0)
+game.p2.karten.size
+
+game.add("midstack", R0)
+game.midCard.karten.size
+game.midCard.karten(0)
+
+game.add("hs", XX)
+
+val game1 = Game("player1", "player2", 1)
+game1.midCard.karten = game.midCard.karten.updated(0, R1)
+game1.toString()
+game1.p1.karten
+game1.p1.karten = game.p1.karten.updated(0, B0)
+game1.p2.karten = game.p2.karten.updated(0, G0)
+game1.p1.karten
+game1.midCard.karten
+game1.toString()
