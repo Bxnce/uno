4789f53acce43484ca0682f89c15ee1ed1e69bcb
haasentimo <haasentimo@gmail.com>
2021-11-18 13:31:49 +0100

    tui

--- src/main/scala/de/htwg/se/uno/aview/TUI.scala
+++ src/main/scala/de/htwg/se/uno/aview/TUI.scala
@@ -8,6 +8,9 @@ import util.Observer
 import Console.{RED, GREEN, RESET}
 
 class TUI(controller: Controller) extends Observer:
+  val ERROR = -1
+  val EXIT = 0
+  val SUCCESS = 1
   controller.add(this)
   print(
     "\n\nWillkommen zu Uno! Um zur Uebersicht der Befehle zu kommen bitte help eingeben\n\n"
@@ -25,13 +28,54 @@ class TUI(controller: Controller) extends Observer:
       )
     )
 
-  def run(input: String): Unit =
+  def run(input: String) =
+    convertinputString(input) match
+      case ERROR   => printhelp()
+      case EXIT    => print("Goodbye\n")
+      case SUCCESS => print("\n\n")
+      case _       => "Hier sollten sie nicht hinkommen"
+
+  def convertinputString(input: String): Int =
+    if (input.size == 0)
+      print("Keine Eingabe!\n")
+      return ERROR
     val in = input
     val innew = in.split(" ")
     innew(0) match
-      case "exit" | "q" => return
+      case "exit" | "q" => return EXIT
       case "help" | "h" =>
-        Console.print(s"""${GREEN}
+        printhelp()
+        return SUCCESS
+      case "add" =>
+        if (innew.size == 3) {
+          val p = innew(1)
+          val c = innew(2)
+          controller.add(p, c)
+          return SUCCESS
+        } else {
+          Console.println(s"${RED}Falscher take Aufruf!${RESET}")
+          return ERROR
+        }
+
+      case "take" | "+" =>
+        if (innew.size == 2) {
+          controller.take(innew(1))
+        } else {
+          controller.take()
+        }
+        return SUCCESS
+      case "place" | "-" =>
+        controller.place(innew(1).toInt - 1)
+        return SUCCESS
+      case "next" | "n" =>
+        controller.next()
+        return SUCCESS
+      case _ =>
+        print("Falsche Eingabe, es gibt folgende Befehle: \n")
+        return ERROR
+
+  def printhelp() =
+    Console.print(s"""${GREEN}
               Befehlsuebersicht fuer Uno:
               - help | h                       : zeigt alle Befehle fuer Uno
               - exit | q                       : verlaesst das Spiel
@@ -40,27 +84,6 @@ class TUI(controller: Controller) extends Observer:
               - place <index> | - <index>      : Legt die Karte an diesem Index auf den Spielstapel
               - next | n                       : Beendet den Zug, der naechste Spieler ist dran       
               ${RESET}""" + "\n")
-      case "add" =>
-        if (innew.size == 3) {
-          val p = innew(1)
-          val c = innew(2)
-          controller.add(p, c)
-        } else {
-          Console.println(s"${RED}Falscher take Aufruf!${RESET}")
-        }
-      case "take" | "+" =>
-        if (innew.size == 2) {
-          controller.take(innew(1))
-        } else {
-          controller.take()
-        }
-      case "place" | "-" =>
-        controller.place(innew(1).toInt - 1)
-      case "next" | "n" =>
-        controller.next()
-      case _ =>
-        println("Ungültige Eingabe, versuchen sie help")
-    print("\n\n")
 
   override def update: Unit =
     print(controller.toString())
--- src/test/scala/de/htwg/se/uno/aview/TUISpec.scala
+++ src/test/scala/de/htwg/se/uno/aview/TUISpec.scala
@@ -21,12 +21,12 @@ class TUISpec extends AnyWordSpec {
     controller.game.p2.karten = controller.game.p2.karten.updated(0, G0)
     controller.game.p2.karten = controller.game.p2.karten.updated(1, G1)
     controller.game.p2.karten = controller.game.p2.karten.updated(2, G2)
+    val tui = TUI(controller)
 
     "created with the given parameters " should {
       "have the following values" in {
-        val tui = TUI(controller) shouldEqual (
-          "\n\nWillkommen zu Uno! Um zur Uebersicht der Befehle zu kommen bitte help eingeben\n\n" +
-            "Bence\n" +
+        controller.toString shouldEqual (
+          "Bence\n" +
             "+--+--+--+\n" +
             "|R0|R1|R2|\n" +
             "+--+--+--+\n" +
@@ -40,7 +40,33 @@ class TUISpec extends AnyWordSpec {
             "+--+--+--+\n" +
             "Timo\n"
         )
+        //vllt einfach einen case new bei dem dann erst ein neues Spiel erstellt wird.
       }
+      "have a method run(String) that executes the commands" in {
+        tui.convertinputString("") shouldBe (tui.ERROR)
+        tui.convertinputString("a") shouldBe (tui.ERROR)
+        tui.convertinputString("add p1") shouldBe (tui.ERROR)
+        tui.convertinputString("help") shouldBe (tui.SUCCESS)
+
+        tui.convertinputString("add p1 R3") shouldBe (tui.SUCCESS)
+        tui.convertinputString("add p2 G3") shouldBe (tui.SUCCESS)
+        controller.game.p1.karten.size shouldBe (4)
+        controller.game.p2.karten.size shouldBe (4)
+        controller.game.p1.karten(3) shouldBe (R3)
+        controller.game.p2.karten(3) shouldBe (G3)
+
+        tui.convertinputString("+") shouldBe (tui.SUCCESS)
+        controller.game.p1.karten.size shouldBe (5)
+
+        tui.convertinputString("- 1") shouldBe (tui.SUCCESS)
+        controller.game.p1.karten.size shouldBe (4)
+        controller.game.midCard.karten(0) shouldBe (R0)
+
+        tui.convertinputString("n") shouldBe (tui.SUCCESS)
+        controller.game.playerDiff shouldBe (1)
+      }
+      "Have a method printhelp() that prints out the help message" in {}
+      "override th method update" in {}
     }
   }
 }
