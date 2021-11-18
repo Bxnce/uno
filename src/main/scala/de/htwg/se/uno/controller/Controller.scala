dfc0ae8142dc7812bdb011f5a8821e363a59c835
haasentimo <haasentimo@gmail.com>
2021-11-18 15:01:47 +0100

    ControllerSpec

--- src/main/scala/de/htwg/se/uno/controller/Controller.scala
+++ src/main/scala/de/htwg/se/uno/controller/Controller.scala
@@ -7,7 +7,7 @@ import util.Observable
 import Console.{RED, RESET}
 
 case class Controller(var game: Game) extends Observable:
-  def add(p: String, karte: String) =
+  def add(p: String, karte: String): Int =
     val err = game.add(p, karte)
     if (err == -3) {
       Console.println(s"${RED}!!!Falschen Namen eingegeben!!!${RESET}")
@@ -18,28 +18,22 @@ case class Controller(var game: Game) extends Observable:
     } else {
       notifyObservers
     }
+    return err
 
-  def take(player: String) =
+  def take(player: String): Int =
     val err = game.take(player)
     if (err == -3) {
       Console.println(s"${RED}!!!Falschen Namen eingegeben!!!${RESET}")
-    } else if (err == -2) {
-      Console.println(s"${RED}!!!Karte ist nichtmehr im Stack!!!${RESET}")
-    } else if (err == -1) {
-      Console.println(s"${RED}!!!Ungültige Karte!!!${RESET}")
     } else {
       notifyObservers
     }
+    return err
 
-  def take() =
+  def take(): Int =
     val err = game.take()
-    if (err == -2) {
-      Console.println(s"${RED}!!!Karte ist nichtmehr im Stack!!!${RESET}")
-    } else if (err == -1) {
-      Console.println(s"${RED}!!!Ungültige Karte!!!${RESET}")
-    } else {
-      notifyObservers
-    }
+    notifyObservers
+    return err
+
   def place(ind: Int) =
     game.place(ind)
     notifyObservers
--- src/test/scala/de/htwg/se/uno/controller/ControllerSpec.scala
+++ src/test/scala/de/htwg/se/uno/controller/ControllerSpec.scala
@@ -10,17 +10,42 @@ import model.Card._
 class ControllerSpec extends AnyWordSpec {
   "Controller" should {
     val game = new Game("p1", "p2", 0)
-    game.midCard.karten = game.midCard.karten.updated(0, R0)
     val c = new Controller(game)
     "contain a game" in {
       c.game.p1.karten.size shouldBe (0)
       c.game.p2.karten.size shouldBe (0)
       c.game.midCard.karten.size shouldBe (1)
-      c.game.midCard.karten(0) shouldBe (R0)
     }
-    "hava a method add(String, String), that calls the add function in game" in {
-      c.add("p1", "B0")
-      c.add("p4", "G0") shouldEqual ("!!!Ungültige Karte!!!\n")
+    "have a method add(String, String), that calls the add function in game" in {
+      //dafür sorgen dass B0 nicht auf dem Spielstapel liegt
+      val tmp = game.midCard.karten(0)
+      game.add("midstack", Y9)
+      game.midCard.removeInd(0)
+      game.cardStack.cards =
+        game.cardStack.cards + (tmp -> ((game.cardStack.cards(tmp) + 1)))
+      game.midCard.karten.size shouldBe (1)
+      game.midCard.karten(0) shouldBe (Y9)
+
+      c.add("p1", "B0") shouldBe (0)
+      c.add("p4", "G0") shouldBe (-3)
+      c.add("p1", "B0") shouldBe (0)
+      c.add("p1", "B0") shouldBe (-2)
+      c.add("p2", "B1") shouldBe (0)
+      c.add("p1", "Hallo") shouldBe (-1)
+    }
+    "have a method take(String), that calls the take(String) function in game" in {
+      c.take("p1") shouldBe (0)
+      c.take("p2") shouldBe (0)
+      c.take("p3") shouldBe (-3)
+    }
+    "have a method take(), that calls the take() function in game" in {
+      c.game.playerDiff shouldBe (0)
+      c.take() shouldBe (0)
+      c.next()
+      c.take() shouldBe (0)
+    }
+    "override th method toString" in {
+      c.toString shouldEqual (game.toString)
     }
   }
 }
