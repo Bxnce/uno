023b34384ac1fc015e0e5b653770f42ba81693fc
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2021-11-25 22:21:14 +0100

    test

--- src/main/scala/de/htwg/se/uno/util/Strategy.scala
+++ src/main/scala/de/htwg/se/uno/util/Strategy.scala
@@ -5,34 +5,26 @@ import model.Game._
 import model.Game
 import model.CardLayout._
 
-object Strategy {}
+trait Strategy {
+  def execute():Unit
+}
 
-object DeathToStrategy extends App {
 
-  def add(a: Int, b: Int) = a + b
-  def subtract(a: Int, b: Int) = a - b
-  def multiply(a: Int, b: Int) = a * b
+case class pick(g: Game) extends Strategy {
+  g.currentstate match
+    
 
-  def execute(callback: (Int, Int) => Int, x: Int, y: Int) = callback(x, y)
-
-  println("Add:      " + execute(add, 3, 4))
-  println("Subtract: " + execute(subtract, 3, 4))
-  println("Multiply: " + execute(multiply, 3, 4))
 
 }
 
-/*
-case class Strat(var game: Game) extends Strategy {
 
-  override def execute(): Unit =
-    if (game.currentstate == game.p1s)
-      return game.p1.getName() + eol + game.p1.print() + eol + game.midCard
-        .print() + eol + game.p2
-        .printFiller() + game.p2
-        .getName() + eol
-    else if (game.currentstate == game.p2s)
-      print("p2")
-    else if (game.currentstate == game.p1n || game.currentstate == game.p2n)
-      print("zwi")
+case class stratP1(g: Game) extends Strategy {
+
+
+
 }
- */
+
+
+
+
+
