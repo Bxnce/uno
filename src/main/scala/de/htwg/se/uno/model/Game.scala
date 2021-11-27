7b254211ac9636ae10cfd507e2def4f640e89a3c
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2021-11-27 11:49:56 +0100

    fixes

--- src/main/scala/de/htwg/se/uno/model/Game.scala
+++ src/main/scala/de/htwg/se/uno/model/Game.scala
@@ -68,6 +68,13 @@ case class Game(player1: String, player2: String, kartenAnzahl: Int):
     } else {
       return -3;
     }
+
+  def addTest(p: String, card: Card): Int =
+    cardStack.cards = cardStack.cards + (midCard
+      .karten(0) -> (cardStack.cards(midCard.karten(0)) + 1))
+    midCard.karten.updated(0, card)
+    cardStack.cards = cardStack.cards + (card -> (cardStack.cards(card) - 1))
+    return 0
   //zieht eine zufällige Karte vom Stack und gibt sie dem Spieler auf die Hand -> dekrementiert die Anzahl der Karte auf dem Stack
   def take(player: String): Int =
     val rnd = r.nextInt(cardsInDeck - 1)
