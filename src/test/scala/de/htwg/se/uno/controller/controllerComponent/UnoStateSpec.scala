c2e264485390d6fd14662e70861741b897c6f3d2
haasentimo <haasentimo@gmail.com>
2022-01-09 12:03:48 +0100

    State Test

--- src/test/scala/de/htwg/se/uno/controller/controllerComponent/UnoStateSpec.scala
+++ src/test/scala/de/htwg/se/uno/controller/controllerComponent/UnoStateSpec.scala
@@ -19,6 +19,7 @@ class UnoStateSpec extends AnyWordSpec {
     val placeC = UnoCommand(0, c)
     val winC = UnoCommand(c, "win")
     val nextC = UnoCommand(c, "next")
+
     "player1State should have a method handle() that matches the commands" in {
       c.game = player1State.handle(takeC)
       c.game.pList(0).karten.size shouldBe (2)
@@ -27,20 +28,28 @@ class UnoStateSpec extends AnyWordSpec {
       c.game.pList(0).karten.size shouldBe (1)
       c.game.midCard.karten(0) shouldEqual (B0)
 
+      c.game = player1State.handle(winC)
+      c.game.ERROR shouldBe (-1)
+
       c.game = player1State.handle(nextC)
       c.game.currentstate shouldBe (between12State)
     }
+
     "player2State should have a method handle() that matches the commands" in {
-
-      c.game = player2State.handle(placeC)
-      c.game.pList(1).karten.size shouldBe (0)
-      c.game.midCard.karten(0) shouldEqual (G0)
-
-      //c.game = player2State.handle(winC)    //bei win bekommt man readlines
+      c.game = player2State.handle(winC)
+      c.game.ERROR shouldBe (-1)
 
       c.game = player2State.handle(nextC)
       c.game.currentstate shouldBe (between21State)
+
+      c.next()
+      c.next()
+      c.next()
+
+      c.game = player2State.handle(placeC)
+      c.game.currentstate shouldBe (winState)
     }
+
     "between12State should have a method handle() that matches the commands" in {
       c.game = between12State.handle(takeC)
       c.game.ERROR shouldBe (-1)
@@ -55,6 +64,20 @@ class UnoStateSpec extends AnyWordSpec {
       c.game.ERROR shouldBe (0)
     }
     "between21State should have a method handle() that matches the commands" in {
+      c.game = between21State.handle(takeC)
+      c.game.ERROR shouldBe (-1)
+
+      c.game = between21State.handle(placeC)
+      c.game.ERROR shouldBe (-1)
+
+      c.game = between21State.handle(winC)
+      c.game.ERROR shouldBe (-1)
+
+      c.game = between21State.handle(nextC)
+      c.game.ERROR shouldBe (0)
+    }
+
+    "winState should have a method handle() that matches the commands" in {
       c.game = between21State.handle(takeC)
       c.game.ERROR shouldBe (-1)
 
