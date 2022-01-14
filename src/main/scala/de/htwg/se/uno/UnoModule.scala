dda659aa39d8f7231fc55072ef14a9e97813fedb
Bence Stuhlmann <bence.stuhlmann@gmail.com>
2022-01-14 08:53:38 +0100

    DI

--- src/main/scala/de/htwg/se/uno/UnoModule.scala
+++ src/main/scala/de/htwg/se/uno/UnoModule.scala
@@ -4,17 +4,18 @@ import com.google.inject.{AbstractModule, Guice, Inject}
 import net.codingwell.scalaguice.ScalaModule
 import controller.controllerComponent.controllerInterface
 import controller.controllerComponent.controllerBaseImpl.Controller
+import controller.controllerComponent.controllerBaseImpl.between21State
 import model.gameComponent.gameInterface
 import model.gameComponent.gameBaseImpl.Game
 
-class UnoModule extends AbstractModule with ScalaModule {
+class UnoModule extends AbstractModule {
 
-  def configure(): Unit = {
-    bind[gameInterface].to[Game]
-    bind[controllerInterface].to[Controller]
-    bind[gameInterface]
+  override def configure(): Unit = {
+    bind(classOf[gameInterface]).to(classOf[Game])
+    bind(classOf[controllerInterface]).to(classOf[Controller])
+    /*bind(classOf[gameInterface])
       .annotatedWithName("start")
-      .toInstance(new Game("place_h", "place_h", between21State))
+      .toInstance(new Game("place_h", "place_h", between21State))*/
   }
 
 }
